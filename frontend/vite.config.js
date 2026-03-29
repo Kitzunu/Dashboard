e8790773e39f039cd5c3cc1bd33c2a65f6340038
Benjamin Jackson <38561765+heyitsbench@users.noreply.github.com>
2026-03-29 17:01:24 -0400

    Init.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
+  server: { host: 0.0.0.0 // todo: make these read from config?
+            port: 5173 },
 });
