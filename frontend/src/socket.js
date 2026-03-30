902be6e76765bdeba9385733f3d0bc677dc11ba5
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2026-03-30 21:59:29 +0200

    Revert "Default Vite backend URL to 0.0.0.0 rather than localhost. (#3)"
    This reverts commit 5ba2c8123347112e7ae2938236d58d610b793304.

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://0.0.0.0:3001';
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
 
 let socket = null;
 
